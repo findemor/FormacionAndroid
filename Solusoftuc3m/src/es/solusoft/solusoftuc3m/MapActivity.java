f2fe12903bdae63024ede2f5ab3bd23620bdb4ef
Findemor <findemor@gmail.com>
2012-10-19 13:43:07 +0200

    Centra el mapa en la ultima posición del dispositivo
    No actualiza si nos movemos.

--- Solusoftuc3m/src/es/solusoft/solusoftuc3m/MapActivity.java
+++ Solusoftuc3m/src/es/solusoft/solusoftuc3m/MapActivity.java
@@ -1,5 +1,12 @@
 package es.solusoft.solusoftuc3m;
 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 
@@ -16,11 +23,29 @@ import android.view.Menu;
  */
 public class MapActivity extends com.google.android.maps.MapActivity {
 	
+	private LocationManager mLocationManager;
+    private MapView mMapView;
+	private MapController mMapController;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
         
+        //Obtenemos las referencias de la vista
+        mMapView = (MapView)findViewById(R.id.mapview);
+        mMapController = mMapView.getController();
+        
+        //Obtenemos la referencia del LocationManager del sistema
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+              
+        //Obtenemos la ultima posici—n cacheada por el dispositivo
+        String locationProvider = LocationManager.NETWORK_PROVIDER; // Or use LocationManager.GPS_PROVIDER
+        Location lastKnownLocation = mLocationManager.getLastKnownLocation(locationProvider);
+
+        //Centramos el mapa
+        mMapController.setZoom(20); //Fixed Zoom Level
+        centerLocation(lastKnownLocation);
     }
 
     @Override
@@ -32,4 +57,18 @@ public class MapActivity extends com.google.android.maps.MapActivity {
 	@Override
 	protected boolean isRouteDisplayed() { return false; }
     
+	
+	
+	/**
+	 * Centra el mapa en la posicion especificada
+	 * @param loc
+	 */
+	 private void centerLocation(Location loc)
+	 {
+	     GeoPoint geoPoint = new GeoPoint(
+	     	(int)(loc.getLatitude()*1000000),
+	     	(int)(loc.getLongitude()*1000000));
+		 
+		 mMapController.animateTo(geoPoint);
+	 };
 }
