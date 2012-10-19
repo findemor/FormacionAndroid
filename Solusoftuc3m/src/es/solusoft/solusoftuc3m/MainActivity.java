d8c50f08ee6cad9dce46dcde2eb9c320faecfa78
Findemor <findemor@gmail.com>
2012-10-19 13:03:13 +0200

    Add control map y permisos necesarios
    Añade el control mapa y los permisos necesarios para su ejecucion.

--- Solusoftuc3m/src/es/solusoft/solusoftuc3m/MainActivity.java
+++ Solusoftuc3m/src/es/solusoft/solusoftuc3m/MainActivity.java
@@ -4,11 +4,8 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-import android.support.v4.app.NavUtils;
 
 /**
  * Actividad principal para el codigo de ejemplo del seminario Android.
--- Solusoftuc3m/src/es/solusoft/solusoftuc3m/MapActivity.java
+++ Solusoftuc3m/src/es/solusoft/solusoftuc3m/MapActivity.java
@@ -1,16 +1,20 @@
 package es.solusoft.solusoftuc3m;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
 
 /**
  * Actividad mapa para el codigo de ejemplo del seminario Android.
  * Mapa + localizacion
  * @author findemor mgarcia@solusoft.es
- *
+ * 
+ * Notas:
+ * Obtener un apiKey para Google Maps: 
+ * 		https://developers.google.com/maps/documentation/android/mapkey
+ * Acerca del keystore de desarollador necesario para obtener el apiKey:
+ *		http://developer.android.com/intl/es/tools/publishing/app-signing.html
  */
-public class MapActivity extends Activity {
+public class MapActivity extends com.google.android.maps.MapActivity {
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,8 @@ public class MapActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+
+	@Override
+	protected boolean isRouteDisplayed() { return false; }
     
 }
